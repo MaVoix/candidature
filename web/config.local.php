e151617f592273710435076d74ff6d1146d7f749
OceanBigOne <clement@garrot.org>
2017-02-24 20:26:02 +0100

    Update gitignore : ignore config.local.php

--- web/config.local.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$_CONFIG["twig_auto_reload"]                 = true; //set true to disable cache twig
-$_CONFIG["environment"]                     = "local";
-$_CONFIG["urlSite"]                         = "http://candidaturemavoix:8080/";
