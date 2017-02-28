f38e131ed309354825ba552e92061ec220edc5e1
OceanBigOne <clement@garrot.org>
2017-02-28 22:03:43 +0100

    #16 Add values on export (DONE)

--- pages/admin/candidature/export.php
+++ pages/admin/candidature/export.php
@@ -4,8 +4,10 @@ require_once "list.php";
 
 $aDataScript=array();
 
-$sCSV="nom;prenom;email;tel;ville;cp;en ligne;attestation;".";\n";
+$sCSV="civilite;nom;prenom;email;tel;ville;cp;en ligne;attestation valide;Carte ID valide;extrait judiciaire valide;".";\n";
 foreach($aObj as $oCandidat){
+    $sCSV.=$oCandidat->getCivility();
+    $sCSV.=";";
     $sCSV.=$oCandidat->getName();
     $sCSV.=";";
     $sCSV.=$oCandidat->getFirstname();
@@ -21,7 +23,12 @@ foreach($aObj as $oCandidat){
     $sCSV.=($oCandidat->getState()=="onine")?'OUI':'NON';
     $sCSV.=";";
     $sCSV.=($oCandidat->getIs_certificate()==1)?'OUI':'NON';
+    $sCSV.=";";
+    $sCSV.=($oCandidat->getIs_idcard()==1)?'OUI':'NON';
+    $sCSV.=";";
+    $sCSV.=($oCandidat->getIs_criminal_record()==1)?'OUI':'NON';
     $sCSV.=";\n";
+
 
 }
 
