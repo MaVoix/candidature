8c49604aede30629d448db2f902b14e9aecf29e1
OceanBigOne <clement@garrot.org>
2017-03-15 23:14:06 +0100

    #35 : Test sur la page candidatures, Save et corrections sql

---
+++ pages/admin/cms/save.php
@@ -0,0 +1,36 @@
+<?php
+
+$aResponse = array();
+$aResponse["type"] = "message";
+
+$aResponse["message"] = array();
+$aResponse["message"]["title"]="Erreur";
+$aResponse["message"]["type"]="error";
+$aResponse["message"]["text"]="Un problème s'est produit lors de l'enregistrement.";
+$aResponse["durationMessage"] = "3000";
+$aResponse["durationRedirect"] = "1";
+$aResponse["durationFade"] = "500";
+$aResponse["required"] = array();
+
+
+
+
+if(isset($_POST["ref"])){
+    $aResponse["message"]["text"]="01";
+    $oListeCms=new CmsListe();
+    $oListeCms->applyRules4GetBlock($_POST["ref"]);
+    $aCms= $oListeCms->getPage();
+    $aResponse["message"]["text"]="02";
+    if(count($aCms)==1){
+        $aResponse["message"]["text"]="03";
+        $oCms= new Cms(array("id"=>$aCms[0]["id"]));
+        $aResponse["type"] = "no-reponse";
+        $oCms->setContent($_POST["content"]);
+        $oCms->setDate_amended(date("Y-m-d H:i:s"));
+        $oCms->save();
+
+    }
+}
+
+//return
+$aDataScript['data'] = json_encode($aResponse);
