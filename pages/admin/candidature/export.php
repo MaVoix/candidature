32cfae3f06a41c515adc8cec1661749e167bd5e2
Clement <clement@garrot.org>
2017-04-01 15:59:44 +0200

    Ajout du champ lien dans l'export CSV #52

--- pages/admin/candidature/export.php
+++ pages/admin/candidature/export.php
@@ -4,7 +4,7 @@ require_once "list.php";
 
 $aDataScript=array();
 
-$sCSV="civilite;nom;prenom;email;tel;ville;cp;en ligne;attestation valide;Carte ID valide;extrait judiciaire valide;".";\n";
+$sCSV="civilite;nom;prenom;email;tel;ville;cp;en ligne;attestation valide;Carte ID valide;extrait judiciaire valide;lien edition;".";\n";
 foreach($aObj as $oCandidat){
     $sCSV.=$oCandidat->getCivility();
     $sCSV.=";";
@@ -27,6 +27,8 @@ foreach($aObj as $oCandidat){
     $sCSV.=($oCandidat->getIs_idcard()==1)?'OUI':'NON';
     $sCSV.=";";
     $sCSV.=($oCandidat->getIs_criminal_record()==1)?'OUI':'NON';
+    $sCSV.=";";
+    $sCSV.=ConfigService::get("urlSite")."/candidature/form.html?id=".$oCandidat->getId()."&key=".$oCandidat->getKey_edit();
     $sCSV.=";\n";
 
 
