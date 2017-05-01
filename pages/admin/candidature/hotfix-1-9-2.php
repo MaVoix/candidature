f84462262b063b0fcc941bb8343889eb58356bc5
Clement <clement@garrot.org>
2017-05-01 20:21:43 +0200

    script de récuperation de fichier

--- pages/admin/candidature/hotfix-1-9-2.php
+++ pages/admin/candidature/hotfix-1-9-2.php
@@ -3,13 +3,49 @@ $aDataScript["out"] = "";
 global $nCountFiles;
 $nCountFiles=0;
 $nCountCandidats=0;
-
-function testPath($sPath){
+if(!isset($_GET["action"])){
+    $_GET["action"]='';
+}
+function testPath($sPath,$sField,$nId){
     global $nCountFiles;
     $sOut="";
     if(!file_exists($sPath) && $sPath!=""){
         $sOut.="<div> IMPOSSIBLE DE TROUVER : ".$sPath."</div>";
         $nCountFiles++;
+        if( $_GET["action"]=="tentative-recup" ||  $_GET["action"]=="recup" ){
+            $sSql="SELECT $sField FROM `candidature-backup` WHERE id='".$nId."'";
+            $stmt=DbLink::getInstance()->prepare($sSql);
+            $stmt->execute(array());
+            $data=$stmt->fetchAll();
+            if(count($data)){
+                $sFile=$data[0][$sField];
+                if(file_exists($sFile)){
+                    $sOut.="<div>--- Fichier récupérable : ".$sFile."</div>";
+                    if($_GET["action"]=="recup"){
+                        $candidature=new Candidature(array("id"=>$nId));
+                        $candidature->hydrateFromBDD(array("*"));
+                        switch($sField){
+                            case "path_pic": $candidature->setPath_pic($sFile); break;
+                            case "path_certificate": $candidature->setPath_certificate($sFile); break;
+                            case "path_idcard": $candidature->setPath_idcard($sFile); break;
+                            case "path_idcard_verso": $candidature->setPath_idcard_verso($sFile); break;
+                            case "path_criminal_record": $candidature->setPath_criminal_record($sFile); break;
+                        }
+
+                        //$candidature->save();
+                        $sOut.="<div>------- Fichier récupéré : ".$sFile."</div>";
+
+                    }
+                }else{
+                    $sOut.="<div>--- Fichier irrécupérable </div>";
+                }
+            }
+
+
+
+        }
+
+
     }
     return $sOut;
 }
@@ -23,11 +59,11 @@ if( ConfigService::get("enable-script-hotfix-1-9-2") ) {
     // parcrous les candidatures
     foreach ($aCandidatures as $aCandidature) {
         $sOut="";
-        $sOut.= testPath($aCandidature["path_pic"]);
-        $sOut.= testPath($aCandidature["path_certificate"]);
-        $sOut.= testPath($aCandidature["path_idcard"]);
-        $sOut.= testPath($aCandidature["path_idcard_verso"]);
-        $sOut.= testPath($aCandidature["path_criminal_record"]);
+        $sOut.= testPath($aCandidature["path_pic"],"path_pic",$aCandidature["id"]);
+        $sOut.= testPath($aCandidature["path_certificate"],"path_certificate",$aCandidature["id"]);
+        $sOut.= testPath($aCandidature["path_idcard"],"path_idcard",$aCandidature["id"]);
+        $sOut.= testPath($aCandidature["path_idcard_verso"],"path_idcard_verso",$aCandidature["id"]);
+        $sOut.= testPath($aCandidature["path_criminal_record"],"path_idcard_verso",$aCandidature["id"]);
         if($sOut!=""){
             $nCountCandidats++;
             $aDataScript["out"] .="<hr />CANDIDATURE :".$aCandidature["id"].$sOut;
