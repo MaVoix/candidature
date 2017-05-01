3435acb87ce9c7df63b78108b6120853020f5e7c
Clement <clement@garrot.org>
2017-05-01 19:10:13 +0200

    modification du Script de test de fichiers manquants (compteurs)

--- pages/admin/candidature/hotfix-1-9-2.php
+++ pages/admin/candidature/hotfix-1-9-2.php
@@ -1,10 +1,15 @@
 <?php
 $aDataScript["out"] = "";
+global $nCountFiles;
+$nCountFiles=0;
+$nCountCandidats=0;
 
 function testPath($sPath){
+    global $nCountFiles;
     $sOut="";
     if(!file_exists($sPath) && $sPath!=""){
         $sOut.="<div> IMPOSSIBLE DE TROUVER : ".$sPath."</div>";
+        $nCountFiles++;
     }
     return $sOut;
 }
@@ -24,10 +29,14 @@ if( ConfigService::get("enable-script-hotfix-1-9-2") ) {
         $sOut.= testPath($aCandidature["path_idcard_verso"]);
         $sOut.= testPath($aCandidature["path_criminal_record"]);
         if($sOut!=""){
+            $nCountCandidats++;
             $aDataScript["out"] .="<hr />CANDIDATURE :".$aCandidature["id"].$sOut;
         }
-
     }
+    $aDataScript["out"] .= "<hr /><hr />";
+    $aDataScript["out"] .= "<div> candidatures  : $nCountCandidats</div>";
+    $aDataScript["out"] .= "<div> fichiers  : $nCountFiles</div>";
+
 }else{
     $aDataScript["out"] .= "<div>EXECUTION BLOQUE (cf. fichier de config)</div>";
 }
